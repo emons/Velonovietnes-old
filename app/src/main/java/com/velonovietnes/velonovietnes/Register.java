c73b1bed563f1b30db7e68f4e9f92f2f4d91d9d8
Emils Lipkovs <el12030@lu.lv>
2015-05-19 11:26:34 +0300

    First commit

---
+++ app/src/main/java/com/velonovietnes/velonovietnes/Register.java
@@ -0,0 +1,40 @@
+package com.velonovietnes.velonovietnes;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class Register extends ActionBarActivity implements View.OnClickListener {
+
+    Button bRegister;
+    EditText etUsername, etEmail, etPassword, etPassword2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.bRegister:
+
+
+                break;
+        }
+    }
+}
